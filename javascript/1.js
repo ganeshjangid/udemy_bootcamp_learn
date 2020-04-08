98cf02c33538ec11d9ebcdecc7195de149235a0f
ganesh <webdeveloper.ganesh@gmail.com>
2020-04-08 19:20:28 +0530

    learn

---
+++ javascript/1.js
@@ -0,0 +1,7 @@
+let name="You must prefix your execution of the *.Js file with the node executable to add context, as if to say,";
+
+let limitCnt=250;
+
+console.log(`Character is left ${parseInt(limitCnt-name.length)}`);
+
+console.log(name.slice(0,1));
